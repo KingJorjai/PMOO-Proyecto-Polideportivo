2d6f7eb36bcbcf1a8e157c623eb1f3f9e24406b6
Jorjai <jorge.are.fdz@gmail.com>
2024-04-29 20:23:53 +0200

    Documentación de la clase Exterior

--- src/net/jorjai/packInstalaciones/Exterior.java
+++ src/net/jorjai/packInstalaciones/Exterior.java
@@ -150,15 +150,31 @@ Iterator).
 public abstract class Exterior extends Instalacion {
 	private int anioConstruccion;
 
+	/**
+	 * Constructor de la clase Exterior
+	 * 
+	 * @param nombre           Nombre de la instalación.
+	 * @param anioConstruccion Año de construcción de la instalación.
+	 */
 	public Exterior(String nombre, int anioConstruccion) {
 		super(nombre);
 		this.anioConstruccion = anioConstruccion;
 	}
 
+	/**
+	 * Devuelve el año de construcción de la instalación.
+	 * 
+	 * @return Año de construcción de la instalación.
+	 */
 	public int getAnioConstruccion() {
 		return anioConstruccion;
 	}
 
+	/**
+	 * Establece el año de construcción de la instalación.
+	 * 
+	 * @param anioConstruccion Año de construcción de la instalación.
+	 */
 	public void setAnioConstruccion(int anioConstruccion) {
 		this.anioConstruccion = anioConstruccion;
 	}
@@ -173,8 +189,12 @@ public abstract class Exterior extends Instalacion {
 		System.out.println(inheritancePath());
 	}
 
-	public abstract double precioAlquiler();
-
+	/**
+     * Actualiza los horarios de apertura y cierre de la instalación.
+     * 
+     * @param horaApertura Hora de apertura de la instalación.
+     * @param horaCierre   Hora de cierre de la instalación.
+     */
 	public void actualizarHorarios(int horaApertura, int horaCierre) {
 		if (horaCierre > horaApertura) {
 			setHoraApertura(horaApertura);
