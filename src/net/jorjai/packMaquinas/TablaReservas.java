8e1bd7f58509226fdd257a8ce84d93c10385d55c
Jorjai <jorge.are.fdz@gmail.com>
2024-04-29 18:49:07 +0200

    Documentación de los métodos de la clase TablaReservas

--- src/net/jorjai/packMaquinas/TablaReservas.java
+++ src/net/jorjai/packMaquinas/TablaReservas.java
@@ -151,6 +151,10 @@ public class TablaReservas {
 
 	private boolean[] reservas;
 
+	/**
+	 * Constructor sin parámetros: inicializa el Array de reservas de la máquina,
+	 * marcando todas sus posiciones como libres (NO reservadas).
+	 */
 	public TablaReservas() {
 		this.reservas = new boolean[24];
 		for (int i = 0; i < reservas.length; i++) {
@@ -158,6 +162,14 @@ public class TablaReservas {
 		}
 	}
 
+	/**
+	 * Dado un número, reserva la máquina para la hora correspondiente a dicho
+	 * número. Devuelve el valor true si se ha podido realizar la reserva
+	 * correctamente.
+	 * 
+	 * @param hora Número de la hora a reservar.
+	 * @return true si se ha podido realizar la reserva correctamente.
+	 */
 	public boolean reservar(int hora) {
 		if (hora >= 0 && hora < 24) {
 			if (!reservas[hora]) {
@@ -168,6 +180,11 @@ public class TablaReservas {
 		return false;
 	}
 
+	/**
+	 * Devuelve la cantidad de horas en las que la máquina está reservada.
+	 * 
+	 * @return Cantidad de horas en las que la máquina está reservada.
+	 */
 	public int numeroReservas() {
 		int contador = 0;
 		for (int i = 0; i < reservas.length; i++) {
@@ -178,6 +195,12 @@ public class TablaReservas {
 		return contador;
 	}
 
+	/**
+	 * Devuelve la primera hora a la que la máquina está libre. Si está reservada a
+	 * todas las horas devolverá -1.
+	 * 
+	 * @return Primera hora a la que la máquina está libre o -1 si no hay ninguna.
+	 */
 	public int primeraLibre() {
 		for (int i = 0; i < reservas.length; i++) {
 			if (!reservas[i]) {
@@ -187,6 +210,14 @@ public class TablaReservas {
 		return -1;
 	}
 
+	/**
+	 * Dado un número entero, devuelve si la máquina está libre a la hora
+	 * correspondiente al número dado.
+	 * 
+	 * @param hora Número de la hora a comprobar.
+	 * @return true si la máquina está libre a la hora correspondiente al número
+	 *         dado.
+	 */
 	public boolean estaLibre(int hora) {
 		if (hora >= 0 && hora < 24) {
 			return !reservas[hora];
@@ -194,6 +225,9 @@ public class TablaReservas {
 		return false;
 	}
 
+	/**
+	 * Libera todas las horas de reserva.
+	 */
 	public void descargar() {
 		for (int i = 0; i < reservas.length; i++) {
 			reservas[i] = false;
