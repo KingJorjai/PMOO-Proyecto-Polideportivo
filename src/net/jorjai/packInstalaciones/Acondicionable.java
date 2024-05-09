6c9695afe0757e8d8b46533b67fa3526b9bbe447
Jorjai <jorge.are.fdz@gmail.com>
2024-05-09 22:15:44 +0200

    Implementación y documentación de la interfaz Acondicionable

---
+++ src/net/jorjai/packInstalaciones/Acondicionable.java
@@ -0,0 +1,15 @@
+package net.jorjai.packInstalaciones;
+
+/**
+ * Interfaz que define el comportamiento de las instalaciones que pueden ser
+ * acondicionadas.
+ */
+public interface Acondicionable {
+
+	/**
+	 * Devuelve el precio del acondicionamiento de la instalación.
+	 * 
+	 * @return Precio del acondicionamiento.
+	 */
+	public double getPrecioAcondicionamiento();
+}
